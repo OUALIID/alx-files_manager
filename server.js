0c79c1434815f0a86283ebb29d67ca82052530cd
OUALIID <walidelhadim8@gmail.com>
2024-03-11 01:56:59 +0000

    Add server setup with route initialization

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+import express from 'express';
+import routes from './routes/index.js';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
