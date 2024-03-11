325e0c2ac145cd0fe537b65e0980a44bea1561ff
OUALIID <walidelhadim8@gmail.com>
2024-03-11 02:51:46 +0000

    update this file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const express = require('express');
 
@@ -9,6 +10,8 @@ const router = (app) => {
 
   route.get('/status', (request, response) => AppController.getStatus(request, response));
   route.get('/stats', (request, response) => AppController.getStats(request, response));
+  route.post('/users', (request, response) => UsersController.postNew(request, response));
+  route.get('/users/me', (request, response) => UsersController.getMe(request, response));
 };
 
 export default router;
