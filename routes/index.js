d9e4a3351e1afad0d67031856b4378fa29015412
OUALIID <walidelhadim8@gmail.com>
2024-03-11 03:00:26 +0000

    I have an error and I am trying to fix it

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,47 @@
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import sha1 from 'sha1';
+import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
-const express = require('express');
+const { ObjectId } = require('mongodb');
 
-const router = (app) => {
-  const route = express.Router();
-  app.use(express.json());
-  app.use('/', route);
+class UsersController {
+  static async postNew(request, response) {
+    const userEmail = request.body.email;
+    if (!userEmail) return response.status(400).send({ error: 'Missing email' });
 
-  route.get('/status', (request, response) => AppController.getStatus(request, response));
-  route.get('/stats', (request, response) => AppController.getStats(request, response));
-  route.post('/users', (request, response) => UsersController.postNew(request, response));
-  route.get('/users/me', (request, response) => UsersController.getMe(request, response));
-};
+    const userPassword = request.body.password;
+    if (!userPassword) return response.status(400).send({ error: 'Missing password' });
 
-export default router;
+    const oldUserEmail = await DBClient.db
+      .collection('users')
+      .findOne({ email: userEmail });
+    if (oldUserEmail) return response.status(400).send({ error: 'Already exist' });
+
+    const shaUserPassword = sha1(userPassword);
+    const result = await DBClient.db
+      .collection('users')
+      .insertOne({ email: userEmail, password: shaUserPassword });
+
+    return response
+      .status(201)
+      .send({ id: result.insertedId, email: userEmail });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await DBClient.db
+      .collection('users')
+      .findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = UsersController;
