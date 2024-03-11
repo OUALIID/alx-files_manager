156d104702d75a0bd52c310910fb6e68ca9397bd
OUALIID <walidelhadim8@gmail.com>
2024-03-11 02:40:15 +0000

    try to fix my code

--- utils/db.js
+++ utils/db.js
@@ -3,32 +3,37 @@ const { MongoClient } = require('mongodb');
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
+    this.db = null;
+    MongoClient.connect(`mongodb://${DB_HOST}:${DB_PORT}`, { useUnifiedTopology: true })
+      .then((client) => {
         this.db = client.db(DB_DATABASE);
         this.users = this.db.collection('users');
         this.files = this.db.collection('files');
-      } else {
-        console.log(err.message);
-        this.db = false;
-      }
-    });
+      })
+      .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err.message);
+      });
   }
 
-  isAlive() { return !!this.db; }
+  isAlive() {
+    return !!this.db;
+  }
 
-  async nbUsers() { return this.users.countDocuments(); }
+  async nbUsers() {
+    return this.users ? this.users.countDocuments() : -1;
+  }
 
-  async nbFiles() { return this.files.countDocuments(); }
+  async nbFiles() {
+    return this.files ? this.files.countDocuments() : -1;
+  }
 
   async getUser(query) {
-    const user = await this.db.collection('users').findOne(query);
-    return user;
+    return this.users ? this.users.findOne(query) : null;
   }
 }
 
-module.exports = new DBClient();
+const dbClient = new DBClient();
+module.exports = dbClient;
