184877c14218512c1444d9054b12483bcaded168
OUALIID <walidelhadim8@gmail.com>
2024-03-11 02:09:11 +0000

    Implemented RedisClient class for basic Redis operations

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,31 @@
 const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-
-    // Event listener for handling errors from the Redis client
-    this.client.on('error', (err) => {
-      console.log(err);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
     });
   }
 
   isAlive() {
-    return this.client.connected; // Return the connection status
+    return this.client.connected;
   }
 
   async get(key) {
-    // Return a Promise that resolves with the value retrieved from Redis
-    return new Promise((resolve) => {
-      this.client.get(key, (err, reply) => {
-        resolve(reply); // Resolve the Promise with the reply from Redis
-      });
-    });
+    return this.getAsync(key);
   }
 
-  // Method to asynchronously set a key-value pair in Redis with expiration
-  async set(key, val, duration) {
-    this.client.set(key, val, 'EX', duration); // Set the key-value pair with expiration
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
   }
 
-  // Method to asynchronously delete a key from Redis
   async del(key) {
-    this.client.del(key); // Delete the key from Redis
+    this.client.del(key);
   }
 }
 
-// Create an instance of RedisClient and export it
 const redisClient = new RedisClient();
 module.exports = redisClient;
