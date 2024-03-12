6f91f18624642beb574caf9d01999deb10b23a9a
OUALIID <walidelhadim8@gmail.com>
2024-03-12 00:52:50 +0000

    try to fix my code

--- utils/db.js
+++ utils/db.js
@@ -1,34 +1,33 @@
 import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-
 class DBClient {
   constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
-        this.db = client.db(DB_DATABASE);
-        this.users = this.db.collection('users');
-        this.files = this.db.collection('files');
-      } else {
-        console.log(err.message);
-        this.db = false;
-      }
-    });
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.mongoURI = `mongodb://${this.host}:${this.port}`;
+
+    this.client = new MongoClient(this.mongoURI, { useUnifiedTopology: true });
+    this.client.connect();
+    this.db = this.client.db(this.database);
   }
 
-  isAlive() { return !!this.db; }
+  isAlive() {
+    return this.client.topology.isConnected();
+  }
 
-  async nbUsers() { return this.users.countDocuments(); }
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    const documentsCount = await usersCollection.countDocuments();
+    return documentsCount;
+  }
 
-  async nbFiles() { return this.files.countDocuments(); }
-
-  async getUser(query) {
-    const user = await this.db.collection('users').findOne(query);
-    return user;
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    const documentsCount = await filesCollection.countDocuments();
+    return documentsCount;
   }
 }
 
-module.exports = new DBClient();
+const redisClient = new DBClient();
+export default redisClient;
