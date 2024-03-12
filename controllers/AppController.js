8c358a228e1891940cf8fa4bd3ce168705240922
OUALIID <walidelhadim8@gmail.com>
2024-03-12 00:21:21 +0000

    try to fix my code

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,39 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-class AppController {
-  static getStatus(req, res) {
-    const data = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive(),
-    };
-    return res.status(200).send(data);
-  }
+const UsersController = {
+  async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
 
-  static async getStats(req, res) {
-    const data = {
-      users: await DBClient.nbUsers(),
-      files: await DBClient.nbFiles(),
-    };
-    return res.status(200).send(data);
-  }
-}
+    const usersCollection = await dbClient.db.collection('users');
 
-module.exports = AppController;
+    const retreivedUser = await usersCollection.findOne({ email });
+    if (retreivedUser) return res.status(400).send({ error: 'Already exist' });
+
+    const hashedPassword = sha1(password);
+    const insertedUser = await usersCollection.insertOne({
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).send({ id: insertedUser.insertedId, email });
+  },
+
+  async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const redisUserId = await redisClient.get(`auth_${token}`);
+    if (!redisUserId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(redisUserId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    return res.status(200).send({ id: user._id, email: user.email });
+  },
+};
+
+export default UsersController;
