9a07fdfb1dd5eaa7fda6d979f800d4f800377c15
OUALIID <walidelhadim8@gmail.com>
2024-03-12 01:16:31 +0000

    Create a new user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,41 +1,86 @@
-import sha1 from 'sha1';
+import ObjectId from 'mongodb';
 import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-const AuthController = {
-  async getConnect(req, res) {
-    const basicAuth = req.headers.authorization;
-    if (basicAuth && typeof basicAuth === 'string' && basicAuth.startsWith('Basic ')) {
-      const extracter = basicAuth.split(' ')[1];
-      const decode = Buffer.from(extracter, 'base64').toString('utf-8');
-
-      const [email, password] = decode.split(':');
-      if (!email || !password) return res.status(401).send({ error: 'Unauthorized' });
-
-      const hash = sha1(password);
-
-      const user = await dbClient.db.collection('users').findOne({ email, password: hash });
-      if (!user) return res.status(401).send({ error: 'Unauthorized' });
-
-      const token = uuidv4();
-      const key = `auth_${token}`;
-      await redisClient.set(key, user._id.toString(), 864000);
-      return res.status(200).send({ token });
-    }
-    return res.status(401).send({ error: 'Unauthorized' });
-  },
-
-  async getDisconnect(req, res) {
+const FilesController = {
+  async postUpload(req, res) {
     const token = req.headers['x-token'];
     if (!token) return res.status(401).send({ error: 'Unauthorized' });
 
-    const userId = await redisClient.get(`auth_${token}`);
-    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+    const redisUserId = await redisClient.get(`auth_${token}`);
+    if (!redisUserId) return res.status(401).send({ error: 'Unauthorized' });
 
-    await redisClient.del(`auth_${token}`);
-    return res.status(204).send();
+    const { name } = req.body;
+    if (!name) return res.status(400).send({ error: 'Missing name' });
+
+    const { type } = req.body;
+    const fileTypes = new Set(['file', 'folder', 'image']);
+    if (!type || !fileTypes.has(type)) return res.status(400).send({ error: 'Missing type' });
+
+    const { data } = req.body;
+    if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
+
+    const parentId = req.body.parentId || 0;
+
+    if (parentId) {
+      const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(parentId) });
+      if (!file) return res.status(400).send({ error: 'Parent not found' });
+      if (file.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
+    }
+
+    const isPublic = req.body.isPublic || false;
+
+    const fileToStore = {
+      userId: redisUserId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    if (type === 'folder') {
+      await dbClient.db.collection('files').insertOne(fileToStore);
+      return res.status(201).send({
+        id: fileToStore._id,
+        userId: fileToStore.userId,
+        name: fileToStore.name,
+        type: fileToStore.type,
+        isPublic: fileToStore.isPublic,
+        parentId: fileToStore.parentId,
+      });
+    }
+
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const filePath = `${folderPath}/${uuidv4()}`;
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true }, (error) => {
+        if (!error) return true;
+        return false;
+      });
+    }
+
+    const fileData = Buffer.from(data, 'base64');
+
+    fs.writeFile(filePath, fileData, (error) => {
+      if (!error) return true;
+      return false;
+    });
+
+    fileToStore.localPath = filePath;
+    await dbClient.db.collection('files').insertOne(fileToStore);
+
+    return res.status(201).send({
+      id: fileToStore._id,
+      userId: fileToStore.userId,
+      name: fileToStore.name,
+      type: fileToStore.type,
+      isPublic: fileToStore.isPublic,
+      parentId: fileToStore.parentId,
+    });
   },
 };
 
-export default AuthController;
+export default FilesController;
